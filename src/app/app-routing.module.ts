a0b5ea359a0bd8fe8a38aad373ca3465877506af
Ben Ruwel <benruwel21@gmail.com>
2020-08-29 13:46:24 +0300

    Change font and polish overall look of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProjectsComponent } from './components/projects/projects.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : LandingComponent},
+  {path: 'about', component : AboutComponent},
+  {path: 'projects', component : ProjectsComponent},
+  {path: 'contacts', component : ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
